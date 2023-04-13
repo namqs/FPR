54b5d35cba32a79a82a6235360d92556948e47b9
Natalie Marques <namqsf@gmail.com>
2023-04-13 08:38:39 -0300

    Add files via upload

---
+++ Arquivos/Aula-Ex04.c
@@ -0,0 +1,44 @@
+//copiar um arq pro outro
+#include <stdio.h>
+
+int copiarArquivo (char nomeArqDestino[], char nomeArqOrigem[]);
+
+void main  ()
+{
+	int retorno = copiarArquivo ("novo.txt", "numeros.txt");
+	
+	//veificando retorno
+	if (retorno ==1)
+	{
+		printf("Arquivo copiado com sucesso!");
+	}
+	else
+	{
+		printf("O arquivo n foi copiado com sucesso");
+	}
+}
+
+int copiarArquivo (char nomeArqDestino[], char nomeArqOrigem[])
+{
+	FILE *arqO, *arqD;
+	int numero;
+	
+	//abrindo
+	arqD = fopen (nomeArqDestino, "w"); 
+	arqO = fopen (nomeArqOrigem, "r");
+	
+	//verificando se houve erro na abertura
+	if ((!arqD) || (!arqO))
+	{
+		return 0;
+	} else
+	{
+		while(fscanf (arqO,"%d", &numero !=EOF))
+		{
+			fprintf(arqD, "%d\n", numero);
+		}
+		fclose (arqO);
+		fclose (arqD);
+	}
+	
+}
