0e43336e26cc11a40900831d51f7810c491c3aee
Natalie Marques <namqsf@gmail.com>
2023-05-02 19:04:27 -0300

    Add files via upload

---
+++ Struct/Slide1.c
@@ -0,0 +1,16 @@
+/*Considere a existência de um vetor no qual cada posição pode armazenar o nome do aluno,
+as notas de AV1 e AV2, sua média e a situação (aprovado, reprovado ou em AVF). Pede-se:
+
+A declaração do vetor, assim como de todos os tipos de dados necessários, caso
+existam;
+
+Uma função que leia os dados de todos os alunos da turma;
+
+Uma função que calcule a média de cada aluno (e a armazene no vetor), assim como a
+sua situação;
+
+Uma função que, dado um número inteiro (1-aprovado;2-reprovado;3-em AVF), exiba
+todos os alunos que estão nesta situação.
+
+*/
+
