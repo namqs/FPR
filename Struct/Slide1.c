51b828e38c42cbb146581b6b3fab1593be390119
Natalie Marques <namqsf@gmail.com>
2023-05-04 20:09:59 -0300

    Update Slide1.c

--- Struct/Slide1.c
+++ Struct/Slide1.c
@@ -14,3 +14,70 @@ todos os alunos que estão nesta situação.
 
 */
 
+#include <stdio.h>
+#include <string.h>
+#define tam 5
+void ler (TAluno vetor[], int tam) //função para ler dados
+void CalcularMédiaStatus (TAluno vetor[], int tam) //função para ler dados
+
+typedef struct {
+    char nome[50];
+    float AV1, AV2, media;
+    char status[15];
+} TAluno;
+
+void main ()
+{
+    TAluno aluno[tam];
+    
+        ler(aluno[], tam);
+        CalcularMédiaStatus (Taluno vetor[], int tam)
+}
+
+void ler (TAluno vetor[], int tam) //função para ler dados
+{
+    int i;
+
+        for (i=0;i<tam;i++)
+        {
+            fflush(stdin);
+            printf("Nome: ");
+            gets (vetor[i].nome);
+        }
+
+        for (i=0;i<tam;i++)
+        {
+            fflush(stdin);
+            printf("Av1: ");
+            scanf("%f", &vetor[i].AV1);
+        }
+
+        for (i=0;i<tam;i++)
+        {
+            fflush(stdin);
+            printf("Av2: ");
+            scanf("%f", &vetor[i].AV1);
+        }
+}
+void CalcularMédiaStatus (TAluno vetor[], int tam) //função para ler dados
+{
+    int i;
+
+        for (i=0;i<tam;i++)
+        {
+            vetor[i].media = (vetor[i].AV1 +vetor[i].AV2);
+
+            if (vetor[i].media>=6)
+            {
+                strcpy (vetor[i].status, "APROVADO");
+            }
+                else if (vetor[i].media < 4)
+                {
+                    strcpy (vetor[i].status, "REPROVADO");
+                }
+                else
+                {
+                    strcpy (vetor[i].status, "AVF");
+                }
+        }
+}
