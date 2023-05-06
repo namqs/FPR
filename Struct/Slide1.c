99da6db5f7cb71d213d6c7c75e41870b9f365cb1
Natalie Marques <namqsf@gmail.com>
2023-05-06 20:11:43 -0300

    Update Slide1.c

--- Struct/Slide1.c
+++ Struct/Slide1.c
@@ -13,12 +13,9 @@ Uma função que, dado um número inteiro (1-aprovado;2-reprovado;3-em AVF), exi
 todos os alunos que estão nesta situação.
 
 */
-
 #include <stdio.h>
 #include <string.h>
-#define tam 5
-void ler (TAluno vetor[], int tam) //função para ler dados
-void CalcularMédiaStatus (TAluno vetor[], int tam) //função para ler dados
+#define TAM_ALUNO 5
 
 typedef struct {
     char nome[50];
@@ -26,58 +23,87 @@ typedef struct {
     char status[15];
 } TAluno;
 
+void ler (TAluno vetor[], int tam);
+void CalcularMediaStatus (TAluno vetor[], int tam);
+void ExibirPorStatus (TAluno vetor [], int tam, int sta);
+
 void main ()
 {
-    TAluno aluno[tam];
-    
-        ler(aluno[], tam);
-        CalcularMédiaStatus (Taluno vetor[], int tam)
+    TAluno aluno[TAM_ALUNO];
+    int status;
+
+    ler(aluno, TAM_ALUNO);
+    CalcularMediaStatus (aluno, TAM_ALUNO);
+    printf("Digite o numero correspondente ao o que deseja ver: \n\n");
+    printf("1-Aprovados\n2-Reprovados\n3-AVF\n: ");
+    scanf("%d", &status);
+    ExibirPorStatus (aluno, TAM_ALUNO, status);
+
 }
 
-void ler (TAluno vetor[], int tam) //função para ler dados
-{
+void ler(TAluno vetor[], int tam) {
     int i;
 
-        for (i=0;i<tam;i++)
-        {
-            fflush(stdin);
-            printf("Nome: ");
-            gets (vetor[i].nome);
-        }
+    for (i = 0; i < tam; i++) {
+        fflush(stdin);
+        printf("Nome: ");
+        gets(vetor[i].nome);
+    }
 
-        for (i=0;i<tam;i++)
-        {
-            fflush(stdin);
-            printf("Av1: ");
-            scanf("%f", &vetor[i].AV1);
-        }
+    for (i = 0; i < tam; i++) {
+        fflush(stdin);
+        printf("Av1: ");
+        scanf("%f", &vetor[i].AV1);
+    }
 
-        for (i=0;i<tam;i++)
-        {
-            fflush(stdin);
-            printf("Av2: ");
-            scanf("%f", &vetor[i].AV1);
-        }
+    for (i = 0; i < tam; i++) {
+        fflush(stdin);
+        printf("Av2: ");
+        scanf("%f", &vetor[i].AV2);
+    }
 }
-void CalcularMédiaStatus (TAluno vetor[], int tam) //função para ler dados
-{
+
+void CalcularMediaStatus(TAluno vetor[], int tam) {
     int i;
 
-        for (i=0;i<tam;i++)
-        {
-            vetor[i].media = (vetor[i].AV1 +vetor[i].AV2);
+    for (i = 0; i < tam; i++) {
+        vetor[i].media = (vetor[i].AV1 + vetor[i].AV2);
 
-            if (vetor[i].media>=6)
-            {
-                strcpy (vetor[i].status, "APROVADO");
-            }
-                else if (vetor[i].media < 4)
-                {
-                    strcpy (vetor[i].status, "REPROVADO");
-                }
-                else
-                {
-                    strcpy (vetor[i].status, "AVF");
-                }
+        if (vetor[i].media >= 6) {
+            strcpy(vetor[i].status, "APROVADO");
         }
+        else if (vetor[i].media < 4) {
+            strcpy(vetor[i].status, "REPROVADO");
+        }
+        else {
+            strcpy(vetor[i].status, "AVF");
+        }
+    }
 }
+
+void ExibirPorStatus(TAluno vetor[], int tam, int sta) {
+    char busca[15];
+    int i;
+
+    switch (sta) {
+        case 1:
+            strcpy(busca, "APROVADO");
+            break;
+
+        case 2:
+            strcpy(busca, "REPROVADO");
+            break;
+
+        case 3:
+            strcpy(busca, "AVF");
+            break;
+    }
+
+    for (i = 0; i < tam; i++) {
+        if (strcmp(vetor[i].status, busca) == 0) {
+            printf("\nNome: %s \nAv1: %.1f - Av2: %.1f - Media: %.1f",
+                vetor[i].nome, vetor[i].AV1, vetor[i].AV2, vetor[i].media);
+        }
+    }
+}
+
